d4c11b9a2a3144177984568c5870f65ba04d2883
Pablo Rogers <realpablorogers@gmail.com>
2024-10-13 06:10:23 -0400

    removed unused file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import HeroPage from "@/components/HeroPage";
-import { ThemeToggle } from "@/components/ThemeToggle";
-import MultiStepForm from "@/features/multistepform/MultiStepForm";
-import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return <HeroPage />;
