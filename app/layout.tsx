fbca381225b5734b52a12bacbb517c84bff0b75e
Pablo Rogers <realpablorogers@gmail.com>
2024-10-13 04:14:11 -0400

    remove boxes

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Boxes />
           {children}
         </ThemeProvider>
       </body>
